87f251e31cf8a05950201858a6fa03355e7c79b2
jondatkins <122027005+jondatkins@users.noreply.github.com>
2026-04-24 15:07:32 +0100

    Re-factor to match course solution.

--- main.go
+++ main.go
@@ -21,32 +21,27 @@ func main() {
 	fmt.Printf("Reading data from %s\n", inputFilePath)
 	fmt.Println("=============================================")
 
-	// reader := bufio.NewReader(file)
-	// buffer := make([]byte, 8)
 	currentLine := ""
 	for {
 		buffer := make([]byte, 8, 8)
 		bytesRead, err := file.Read(buffer)
 		if err != nil {
+			if currentLine != "" {
+				fmt.Printf("read: %s\n", currentLine)
+				currentLine = ""
+			}
 			if errors.Is(err, io.EOF) {
 				break
 			}
 			fmt.Printf("error: %s\n", err.Error())
 			break
 		}
-		byteOfFile := string(buffer[:bytesRead])
-		parts := strings.Split(byteOfFile, "\n")
-		if len(parts) < 2 {
-			currentLine += byteOfFile
-		}
-
+		str := string(buffer[:bytesRead])
+		parts := strings.Split(str, "\n")
 		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s\n", currentLine+parts[i])
+			fmt.Printf("read: %s%s\n", currentLine, parts[i])
 			currentLine = ""
-			currentLine += parts[len(parts)-1]
 		}
-	}
-	if len(currentLine) > 0 {
-		fmt.Printf("read: %s\n", currentLine)
+		currentLine += parts[len(parts)-1]
 	}
 }
