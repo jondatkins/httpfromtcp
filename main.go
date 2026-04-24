983e0002abc060e28a4e4b8d009bbea43cc95a28
jondatkins <122027005+jondatkins@users.noreply.github.com>
2026-04-24 15:00:12 +0100

    Reading bytes, printing lines

--- main.go
+++ main.go
@@ -1,33 +1,52 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"os"
+	"strings"
 )
 
+const inputFilePath = "messages.txt"
+
 func main() {
-	file, err := os.Open("messages.txt")
+	file, err := os.Open(inputFilePath)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal("could not open %s: %s\n", inputFilePath, err)
 	}
 	defer file.Close()
 
-	reader := bufio.NewReader(file)
-	buffer := make([]byte, 8)
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("=============================================")
 
+	// reader := bufio.NewReader(file)
+	// buffer := make([]byte, 8)
+	currentLine := ""
 	for {
-		bytesRead, err := reader.Read(buffer)
+		buffer := make([]byte, 8, 8)
+		bytesRead, err := file.Read(buffer)
 		if err != nil {
-			if err.Error() == "EOF" {
+			if errors.Is(err, io.EOF) {
 				break
 			}
-			fmt.Println("Error reading file:", err)
-			return
+			fmt.Printf("error: %s\n", err.Error())
+			break
 		}
-		file_line := string(buffer[:bytesRead])
-		// trimmed_line := strings.TrimSpace(file_line)
-		fmt.Print("read: " + file_line + "\n")
+		byteOfFile := string(buffer[:bytesRead])
+		parts := strings.Split(byteOfFile, "\n")
+		if len(parts) < 2 {
+			currentLine += byteOfFile
+		}
+
+		for i := 0; i < len(parts)-1; i++ {
+			fmt.Printf("read: %s\n", currentLine+parts[i])
+			currentLine = ""
+			currentLine += parts[len(parts)-1]
+		}
+	}
+	if len(currentLine) > 0 {
+		fmt.Printf("read: %s\n", currentLine)
 	}
 }
